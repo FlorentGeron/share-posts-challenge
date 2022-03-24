a2ca9d65a9f25bbce207a9a907b13c7b4a13c6a9
Florent Geron <florent.geron@orange.fr>
2022-03-24 15:45:41 +0100

    controllers ok seed users ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Hello People! Wanna seed?"
+User.destroy_all
+Post.destroy_all
+Comment.destroy_all
+
+puts "OK, I'll start by creating 2 users for you"
+
+user1 = User.new(
+  email: "master@certif.com",
+  nickname: "Master",
+  password: "000000"
+)
+user1.save!
+
+user2 = User.new(
+  email: "rookie@certif.com",
+  nickname: "Rookie",
+  password: "000000"
+)
+user2.save!
+
+puts "Created #{User.count} #{"user".pluralize(User.count)} for you"
